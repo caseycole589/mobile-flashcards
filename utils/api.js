dc4965afcd0b631c54a3221e36130bc96f679af7
Casey Cole <caseycole589@gmail.com>
2018-12-27 17:27:09 -0500

    created colors and api for async storage

---
+++ utils/api.js
@@ -0,0 +1,43 @@
+import { AsyncStorage } from 'react-native';
+import { formatDecks, formatCards, DECKS_STORAGE_KEY } from './helpers';
+import _ from 'lodash';
+
+export function fetchDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDecks);
+}
+
+export function submitDeck(title) {
+  return AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({
+      [title]: {
+        title: title,
+        questions: []
+      }
+    })
+  );
+}
+
+export function fetchCards() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDecks);
+}
+
+export function submitCard(id, card) {
+  AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    let questions = _.find(data, ['title', id]).questions;
+
+    questions.push(card);
+    data[id].questions = questions;
+
+    AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(data), () => {
+      return AsyncStorage.mergeItem(
+        DECKS_STORAGE_KEY,
+        JSON.stringify(data),
+        () => {
+          AsyncStorage.getItem(DECKS_STORAGE_KEY).then(formatDecks);
+        }
+      );
+    });
+  });
+}
